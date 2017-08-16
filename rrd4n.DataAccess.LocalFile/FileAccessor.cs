d7e475ca23473b1b9a1798a0038472f3cdae6de4
Andrew Yourtchenko <ayourtch@gmail.com>
2017-08-16 14:13:34 +0200

    Reformat the file

--- rrd4n.DataAccess.LocalFile/FileAccessor.cs
+++ rrd4n.DataAccess.LocalFile/FileAccessor.cs
@@ -8,61 +8,61 @@ using rrd4n.DataAccess.Interface;
 
 namespace rrd4n.DataAccess.LocalFile
 {
-   public class FileAccessor : RrdDbAccessInterface
-   {
-      public string DataPath { get; set; }
+	public class FileAccessor : RrdDbAccessInterface
+	{
+		public string DataPath { get; set; }
 
-      #region RrdDbAccessInterface Members
+		#region RrdDbAccessInterface Members
 
-      public rrd4n.DataAccess.Data.FetchData GetData(rrd4n.DataAccess.Data.FetchRequest request)
-      {
-         RrdDb rrdDb = null;
-         try
-         {
-            string dataPath;
-            if (DataPath.Contains("${APPDATA}"))
-            {
-               dataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-               dataPath += DataPath.Substring(10);
-            }
-            else
-               dataPath = DataPath;
+		public rrd4n.DataAccess.Data.FetchData GetData(rrd4n.DataAccess.Data.FetchRequest request)
+		{
+			RrdDb rrdDb = null;
+			try
+			{
+				string dataPath;
+				if (DataPath.Contains("${APPDATA}"))
+				{
+					dataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+					dataPath += DataPath.Substring(10);
+				}
+				else
+					dataPath = DataPath;
 
 
-            rrdDb = new RrdDb(dataPath + request.DatabasePath);
-            FetchRequest coreRequest = new FetchRequest(rrdDb.getPath(), request.ConsolidateFunctionName, request.FetchStart, request.FetchEnd, request.Resolution);
-            FetchData coreFetchData = rrdDb.fetchData(coreRequest);
-            
-            return new rrd4n.DataAccess.Data.FetchData(coreFetchData.getArcStep(), coreFetchData.getArcEndTime(), coreFetchData.getDsNames())
-                                                           {
-                                                              Values = coreFetchData.getValues(),
-                                                              Timestamps = coreFetchData.getTimestamps()
-                                                           };
-         }
-         finally
-         {
-            if (rrdDb != null)
-               rrdDb.close();
-         }
-      }
+				rrdDb = new RrdDb(dataPath + request.DatabasePath);
+				FetchRequest coreRequest = new FetchRequest(rrdDb.getPath(), request.ConsolidateFunctionName, request.FetchStart, request.FetchEnd, request.Resolution);
+				FetchData coreFetchData = rrdDb.fetchData(coreRequest);
 
-      public void StoreData(rrd4n.DataAccess.Data.Sample sample)
-      {
-         RrdDb rrdDb = null;
-         try
-         {
-            rrdDb = new RrdDb(DataPath + sample.DatabasePath, false);
-            Sample coreSample = new Sample(rrdDb.getPath(),rrdDb.getDsNames(), sample.Time);
-            coreSample.setValues(sample.Values);
-            rrdDb.store(coreSample);
-         }
-         finally
-         {
-            if (rrdDb != null)
-               rrdDb.close();
-         }
-      }
+				return new rrd4n.DataAccess.Data.FetchData(coreFetchData.getArcStep(), coreFetchData.getArcEndTime(), coreFetchData.getDsNames())
+				{
+					Values = coreFetchData.getValues(),
+					Timestamps = coreFetchData.getTimestamps()
+				};
+			}
+			finally
+			{
+				if (rrdDb != null)
+					rrdDb.close();
+			}
+		}
 
-      #endregion
-   }
+		public void StoreData(rrd4n.DataAccess.Data.Sample sample)
+		{
+			RrdDb rrdDb = null;
+			try
+			{
+				rrdDb = new RrdDb(DataPath + sample.DatabasePath, false);
+				Sample coreSample = new Sample(rrdDb.getPath(), rrdDb.getDsNames(), sample.Time);
+				coreSample.setValues(sample.Values);
+				rrdDb.store(coreSample);
+			}
+			finally
+			{
+				if (rrdDb != null)
+					rrdDb.close();
+			}
+		}
+
+		#endregion
+	}
 }
